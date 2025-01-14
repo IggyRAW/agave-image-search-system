f9f44d1594498048248424f4d23a612a1148084e
Sota Nakamura <s-nakamura@asking.co.jp>
2025-01-14 12:10:09 +0900

    該当データなしの場合の表示分追加

--- src/frontend/src/views/SearchView.vue
+++ src/frontend/src/views/SearchView.vue
@@ -33,13 +33,20 @@ onMounted(() => {
   <v-app>
     <Header @searchList="handleSearchList" />
     <v-main>
+      <!-- 検索バー -->
       <SearchBar @searchList="handleSearchList" />
-      <div style="max-width: 95%; margin: 0 auto; padding: 16px">
+
+      <!-- アイテム -->
+      <div
+        v-if="receivedSearchList.length > 0"
+        style="max-width: 95%; margin: 0 auto; padding: 16px"
+      >
         <v-row>
           <v-col
             v-for="item in receivedSearchList"
             :key="item.id"
             cols="12"
+            xs="6"
             sm="6"
             md="4"
             lg="3"
@@ -48,10 +55,15 @@ onMounted(() => {
             <CardItem :item="item" />
           </v-col>
         </v-row>
-        <v-btn v-if="showScrollButton" class="scroll-to-top" @click="scrollToTop" icon>
-          <v-icon>mdi-arrow-up</v-icon>
-        </v-btn>
       </div>
+      <div v-else class="no-results">
+        <p>該当するアガベがありませんでした</p>
+        <p>キーワードを変更して再検索してください</p>
+      </div>
+      <!-- トップへ移動ボタン -->
+      <v-btn v-if="showScrollButton" class="scroll-to-top" @click="scrollToTop" icon>
+        <v-icon>mdi-arrow-up</v-icon>
+      </v-btn>
     </v-main>
   </v-app>
 </template>
@@ -66,5 +78,16 @@ onMounted(() => {
   border-radius: 50%;
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
   z-index: 1000;
+}
+.no-results {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  min-height: 100px;
+  margin: 3px auto;
+  text-align: center;
+}
+.no-results p {
+  margin: 3px 0;
 }
 </style>
