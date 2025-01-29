c7810e6baf9e50817ab51d94bef644a935066929
Sota Nakamura <s-nakamura@asking.co.jp>
2025-01-29 19:04:48 +0900

    データ管理サイト用のページを追加

--- src/frontend/src/router/index.ts
+++ src/frontend/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SearchView from '../views/SearchView.vue'
+import ManagementView from '@/views/ManagementView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'search',
       component: SearchView,
+    },
+    {
+      path: '/manage',
+      name: 'management',
+      component: ManagementView,
     },
   ],
 })
