ad8703fb9d54078da8e15dbdf81a562b8d472af9
Sota Nakamura <s-nakamura@asking.co.jp>
2025-03-03 13:40:21 +0900

    ビルド設定変更

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import VitePreloadPlugin from 'vite-plugin-preload'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -27,7 +28,8 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      // '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 })
